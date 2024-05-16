0e1972710412178c61c9a40928534d0518182223
Key Clark <aryeh.the.lion@gmail.com>
2024-05-16 17:12:43 -0500

    working branch setup

--- main.go
+++ main.go
@@ -3,28 +3,20 @@ package main
 import "fmt"
 
 func main() {
-
-	// 16 example student names
 	names := []string{
-		"Alex Ryan",
-		"Fernando Valdez",
-		"Javier Rice",
-		"Quique Jefferson",
-		"Nija Desirae",
-		"Chava Rosenwort",
-		"Salaam Muhammad",
-		"Braeden Kincade",
-		"Peng Zhang",
-		"Axe Rivera",
-		"Zander Ofosu",
-		"Nicolas Sosa",
-		"Wendy Daye",
-		"TacoCat Dogod",
-		"Locke Lamora",
-	}
-
-
-		
+		"Ani Subbarao",
+		"Brock Biever",
+		"Crystal EvenRam (Remote)",
+		"Joshua Bass  (Remote)",
+		"Justin Kahn (Remote)",
+		"Kalia Vang",
+		"Luke Edwards",
+		"Michael Lafond",
+		"Sam Shacter",
+		"Sumeiya Abdi (Remote)",
+		"Tierra Byrd",
+		"Tristan Bridges",
+		}
 
 	numberOfPods := 4
 
